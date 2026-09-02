87eda224a3af8b42c0b58450b95893e19dfae536
Shaub Daud <shuakshay@gmai.com>
2026-09-02 15:13:32 +0300

    2.4 Access Modifiers complete

--- src/main/java/com/amigoscode/Application.java
+++ src/main/java/com/amigoscode/Application.java
@@ -1,10 +1,34 @@
 package com.amigoscode;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.*;
+import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Application {
+    enum Gender {
+        MALE,
+        FEMALE;
+
+        enum Color {
+            BLUE,
+            GREEN,
+            RED;
+
+            enum Months {
+                JANUARY,
+                FEBRUARY,
+                MARCH
+            }
+        }
+    }
 
     public static void main(String[] args) {
         System.out.println("Amigoscode Java Exercises");
         System.out.println("Navigate to each package and run the exercises individually.");
+
     }
 
 }
